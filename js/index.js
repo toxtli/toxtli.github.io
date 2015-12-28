e939f1e7fbd0627e64e86ed090caff7ce2f2b630
Carlos Toxtli Hernandez <cthernandez@cthernandez-macbookair.roam.corp.google.com>
2015-12-28 12:34:45 -0500

    New change

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-$(function() {
-	$.getJSON('json/data.json', function(data){
-		$('#Image_Profile').attr('src', data.picture);
-		$('#Content').html(JSON.stringify(data, null, 2));
+var config = {url: 'json/data.json'};
+var app = angular.module('Profile_App', []);
+app.controller('Profile_Controller', function($scope, $http) {
+	$http.get(config.url).success(function (response) {
+		$scope.data = response;
 	});
 });
