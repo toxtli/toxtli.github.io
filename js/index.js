b7cc60f25a539767302276a4f14133b9046049aa
Carlos Toxtli Hernandez <cthernandez@cthernandez-macbookair2.roam.corp.google.com>
2016-04-03 17:57:12 -0400

    New changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var config = {url: 'json/data.json'};
+var config = {url: 'json/profile.json'};
 var app = angular.module('Profile_App', []);
 app.controller('Profile_Controller', function($scope, $http) {
 	$http.get(config.url).success(function (response) {
